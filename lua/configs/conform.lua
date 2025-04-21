020ab527e8befbaf3e87e05ecab3bd4892ba1e1f
DevBaldev <triggerflax@gmail.com>
2025-04-21 05:55:45 +0530

    21/04/25 05:55

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -2,13 +2,12 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     bash = { "shfmt" },
-    python = { "yapf" },
     c = { "clang-format" },
     cpp = { "clang-format" },
+    python = { "isort", "black" },
   },
 
   format_on_save = {
-    -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
